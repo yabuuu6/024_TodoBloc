53d0c8a77c91e42a8185edcf56754bfece38f54e
buyaramadhan <ytchochote@gmail.com>
2025-05-09 01:38:09 +0700

    Menambahkan definisi TodoState beserta turunan TodoInitial, TodoLoading, dan TodoLoaded

---
+++ lib/bloc/todo_state.dart
@@ -0,0 +1,19 @@
+part of 'todo_bloc.dart';
+
+sealed class TodoState {}
+
+final class TodoInitial extends TodoState{}
+
+final class TodoLoading extends TodoState {}
+
+final class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+  DateTime? selectedDate;
+
+  TodoLoaded({
+    required this.todos,
+    required this.selectedDate
+  });
+
+ 
+}
